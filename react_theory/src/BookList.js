115f03e3cfda6f519941683ceb4231abe00cd2b9
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2024-12-05 14:13:43 +0530

    [Sparsh] Add: Creating Components

---
+++ react_theory/src/BookList.js
@@ -0,0 +1,12 @@
+import Book from "./Book";
+import React from "react";
+
+const BookList=()=>{
+    return(
+        <div>
+            <Book />
+            <Book />
+            <Book />
+        </div>
+    )
+}
