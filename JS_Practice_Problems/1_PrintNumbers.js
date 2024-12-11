5300bab5fed34cac4e5ce24f7c6ef614d415b504
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2024-12-11 10:36:53 +0530

    [Sparsh] Add: Print Numbers from 1 to 10

---
+++ JS_Practice_Problems/1_PrintNumbers.js
@@ -0,0 +1,17 @@
+// Print numbers from 1 to 10
+
+// Approach 1 - Using For Loop
+console.log(`Printing Using For Loop`);
+for (let i = 1; i <= 10; i++) {
+    console.log(i);
+}
+console.log();
+
+// Approach 2 - Using While Loop
+console.log(`Printing Using While Loop`);
+let i = 1;
+while (i < 11) {
+    console.log(i);
+    i++;
+}
+console.log();
