a8d10c2f74aa42390bdd780ae7c60e6f8697c2bc
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2024-12-11 10:47:39 +0530

    [Sparsh] Refactor: Update Comments for Multiple Approaches

--- JS_Practice_Problems/3_TableOf7.js
+++ JS_Practice_Problems/3_TableOf7.js
@@ -1,13 +1,13 @@
 // Print the Multiplication Table of 7
 
-// Using For Loop
+// Approach 1 - Using For Loop
 console.log("Printing Using For Loop");
 for (let i = 1; i < 11; i++) {
     console.log(`7 x ${i} = ${7 * i}`);
 }
 console.log();
 
-// Using While Loop
+// Approach 2 - Using While Loop
 console.log("Printing Using While Loop");
 let i = 1;
 while (i < 11) {
