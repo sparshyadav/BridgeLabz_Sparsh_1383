726289c76a10d3e4bc0bf878b91889fbf29d2a9e
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2024-12-11 10:54:42 +0530

    [Sparsh] Add: Print Sum of Numbers from 1 to 10

---
+++ JS_Practice_Problems/5_SumOfNumbers.js
@@ -0,0 +1,19 @@
+// Calculate the Sum of Numbers from 1 to 10
+
+// Approach 1 - Using For Loop
+let sum = 0;
+for (let i = 1; i < 11; i++) {
+    sum += i;
+}
+console.log(`The Sum of Numbers from 1 to 10 is ${sum}`);
+
+// Approach 2 - Putting the Numbers in an Array and Using Reduce
+let arr = [];
+for (let i = 1; i < 11; i++) {
+    arr.push(i);
+}
+
+let reduceSum = arr.reduce((acc, num) => {
+    return acc + num;
+});
+console.log(`The Sum of Numbers from 1 to 10 is ${reduceSum}`);
