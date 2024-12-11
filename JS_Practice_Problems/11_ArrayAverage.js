92f4d8dd7713e10f08e4ed3c1c93f0bae2bf9a64
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2024-12-11 11:30:07 +0530

    [Sparsh] Add: Calculate the Average of an Array

---
+++ JS_Practice_Problems/11_ArrayAverage.js
@@ -0,0 +1,12 @@
+// Calculate the Average of Numbers in an Array 
+
+// Approach - Using Reduce to find Sum, and then finding the Average
+let arr = [32, 646, 23, 75, 43, 23, 54, 23];
+
+let newSum = arr.reduce((acc, num) => {
+    return acc + num;
+})
+
+let average = (newSum / arr.length).toFixed(2);
+
+console.log(`The Average of Arr is: ${average}`);
