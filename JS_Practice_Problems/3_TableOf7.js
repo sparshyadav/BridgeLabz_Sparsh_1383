6a946920ead8ce07e68690f417c1495ee9327b51
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2024-12-11 10:44:13 +0530

    [Sparsh] Add: Print Table of 7

---
+++ JS_Practice_Problems/3_TableOf7.js
@@ -0,0 +1,17 @@
+// Print the Multiplication Table of 7
+
+// Using For Loop
+console.log("Printing Using For Loop");
+for (let i = 1; i < 11; i++) {
+    console.log(`7 x ${i} = ${7 * i}`);
+}
+console.log();
+
+// Using While Loop
+console.log("Printing Using While Loop");
+let i = 1;
+while (i < 11) {
+    console.log(`7 x ${i} = ${7 * i}`);
+    i++;
+}
+console.log();
