1bbf91d73c557df375e1bb19816143e5bc4a6b3d
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2024-12-13 10:43:21 +0530

    [Sparsh] Add: All String Methods in JS

---
+++ StringMethods.js
@@ -0,0 +1,29 @@
+let str = "letsdoleetcode";
+let str1 = "hello";
+let str2 = "world";
+let str3 = "there";
+
+// // charAt(index) - Returns the character at the specified index
+// console.log(str.charAt(2));
+// console.log(str.charAt(5));
+// console.log(str.charAt(12));
+
+
+// // charCodeAt(index) - Returns the ASCII code of the character at the specified index
+// console.log(str.charCodeAt(2));
+// console.log(str.charCodeAt(5));
+// console.log(str.charCodeAt(12));
+
+
+// // string1.concat(string2, string3) - Concats 2 or more string 
+// let concatString = str1.concat(str2);
+// console.log(concatString);
+// concatString = str1.concat(str2, str3);
+// console.log(concatString);
+
+
+// // string.include(substring) - Checks if the string contains a substring
+// let s1 = "code";
+// let s2 = "let's";
+// console.log(str.includes(s1));
+// console.log(str.includes(s2));
